d080fb9178f2bc53e4276eaf6fcd25a596298e98
Aldrin Jerome Almacin <aldrinalmacin@yahoo.com>
2013-02-08 20:44:35 -0500

    Season year now changes based on what season update

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -11,11 +11,21 @@ class AdminController < ApplicationController
       back_to_admin_page_and_say "Invalid confirmation password"
     else
       back_to_admin_page_and_say "Successfully updated"
+      update_the_season
     end
   end
 
   private
   def back_to_admin_page_and_say message
     redirect_to admin_path, :notice => message
+  end
+
+  def update_the_season
+    if @user.season_started
+      @user.update_attributes(:season_started => false)
+      @user.update_attributes(:current_year => @user.current_year += 1) 
+    else
+      @user.update_attributes(:season_started => true)
+    end
   end
 end
